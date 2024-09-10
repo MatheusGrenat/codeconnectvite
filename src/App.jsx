4c5403a088037ddbd3ac49acb3be48c423b8ede7
MatheusGrenat <matheusgrenat@yahoo.com.br>
2024-09-10 19:29:14 -0300

    Criando e estilizando o componente barra de pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import BarraDePesquisa from './componentes/BarraDePesquisa'
 import Sidebar from './componentes/Sidebar'
 
 function App() {
   return (
+    <div>
       <Sidebar />
+      <BarraDePesquisa />
+    </div>
   )
 }
 
